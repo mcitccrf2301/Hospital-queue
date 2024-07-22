1403957d85e35481bebb3ff5107d10a4c8347d7b
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-22 18:44:22 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key"{
+}
+variable "aws_secret_access_key"{
+}
+
+variable "aws_region" {
+    description = "Region in which AWS Resources to be created"
+    type = string
+    default ="us-east-1"
+}
