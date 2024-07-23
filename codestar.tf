5eb035a00f325a97c8442e5f464b04a215d08cc1
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-22 20:09:06 -0400

    Update codestar.tf

--- codestar.tf
+++ codestar.tf
@@ -4,8 +4,8 @@ resource "aws_codestar_project" "mcitccrf2301" {
   description = "mcitccrf2301 Hospital Queue Project"
 }
 
-resource "aws_codestar_github_repository" "Hospital-queue" {
-  project_id  = aws_codestar_project.Hospital_queue.id
+resource "aws_codestar_github_repository" "hospital_queue" {
+  project_id  = aws_codestar_project.mcitccrf2301.id
   repository  = "Hospital-queue"
   owner       = "mcitccrf2301" 
   oauth_token = var.github_token  
