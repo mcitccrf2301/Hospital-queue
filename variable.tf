9671ea83eb006cf0c37fe8b7272d866605264ae3
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-22 20:02:13 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,18 @@
 variable "aws_access_key"{
-type = string
+    type = string
 }
 variable "aws_secret_access_key"{
-type = string
+    type = string
 }
 
 variable "aws_region" {
     description = "Region in which AWS Resources to be created"
     type = string
     default ="us-east-1"
+}
+
+variable "github_token" {
+  description = "GitHub OAuth token"
+  type        = string
+  sensitive   = true
 }
