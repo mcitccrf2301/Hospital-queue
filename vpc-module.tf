5aaa13a22b304335d860bdd93ce0c976a82dda03
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-24 20:06:31 -0400

    Create vpc-module.tf

---
+++ vpc-module.tf
@@ -0,0 +1,42 @@
+////// This was added July 24 2024 - may need some updates
+
+# Create VPC Terraform Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.4.0" #Latest version is 5.9.0 - should we update?
+
+  # VPC Basic Details
+  name = "${var.vpc_name}"
+  cidr = var.vpc_cidr_block
+  azs             = var.vpc_availability_zones
+  public_subnets  = var.vpc_public_subnets
+  private_subnets = var.vpc_private_subnets  
+
+  # Database Subnets
+  database_subnets = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # create_database_internet_gateway_route = true
+  # create_database_nat_gateway_route = true
+  
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = var.vpc_enable_nat_gateway 
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "Public Subnets"
+  }
+  private_subnet_tags = {
+    Type = "Private Subnets"
+  }  
+  database_subnet_tags = {
+    Type = "Private Database Subnets"
+  }
+
+}
