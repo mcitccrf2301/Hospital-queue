72d992d769c1613f8df60beb9ef6069bbd3e47e4
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-28 10:24:59 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+////Below added July 28 2024
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+/////
   }
+
+
   required_version=">=1.6.6" #this version is for Terraform Version, not aws
     cloud {
           organization = "mcitccrf2301"
@@ -12,4 +25,5 @@ terraform {
           name = "Hospital-queue"
       }
     }
+  
 }
