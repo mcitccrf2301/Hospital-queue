cb119aff7b14cceb93eb49f256261e699bcdc617
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-29 20:56:21 -0400

    Update instance_code.tf

--- instance_code.tf
+++ instance_code.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "hospital_queue" {
   ami           = var.ami_for_compute
   instance_type = var.compute_type
-  key_name      = var.instance_key
+  key_name      = "hospital_key_pair"
 
   subnet_id = module.vpc.public_subnets[0]
 
