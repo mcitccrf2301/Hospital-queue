8217024d75c5160d55c2ae4e3d6ad6b8cdc235fd
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-07-29 20:07:22 -0400

    Update instance_code.tf

--- instance_code.tf
+++ instance_code.tf
@@ -2,10 +2,15 @@ resource "aws_instance" "hospital_queue" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
   key_name      = "hospital_key_pair"
-  subnet_id = 
-  security_groups = 
+
+  subnet_id = module.vpc.public_subnets[0]
+
+  vpc_security_group_ids = [
+    aws_security_group.sg_ssh.id,
+    aws_security_group.sg_web.id,
+  ]
+
   associate_public_ip_address = true
-  # Using a file function to load the startup script
   user_data = file("${path.module}/bootstrap_hospital_queue.sh")
 
   tags = {
