f4ad22ed7412c7283529a95c03ebed3a0f407bba
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-08-02 15:11:01 -0400

    Update api_for_boot.tf

--- api_for_boot.tf
+++ api_for_boot.tf
@@ -1,74 +1,56 @@
 resource "aws_api_gateway_rest_api" "hospital_queue_api" {
-  name        = "hospital_queue_api"
-  description = "API for the Hospital Queue System"
+  name = "hospital_queue_api"
 }
 
-resource "aws_api_gateway_resource" "hospital" {
+resource "aws_api_gateway_resource" "submit_resource" {
   rest_api_id = aws_api_gateway_rest_api.hospital_queue_api.id
   parent_id   = aws_api_gateway_rest_api.hospital_queue_api.root_resource_id
-  path_part   = "hospital"
+  path_part   = "submit"
 }
 
-resource "aws_api_gateway_method" "post_hospital" {
+resource "aws_api_gateway_resource" "check_queues_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hospital_queue_api.id
+  parent_id   = aws_api_gateway_rest_api.hospital_queue_api.root_resource_id
+  path_part   = "check_queues"
+}
+
+resource "aws_api_gateway_method" "submit_method" {
   rest_api_id   = aws_api_gateway_rest_api.hospital_queue_api.id
-  resource_id   = aws_api_gateway_resource.hospital.id
+  resource_id   = aws_api_gateway_resource.submit_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "post_hospital" {
+resource "aws_api_gateway_method" "check_queues_method" {
   rest_api_id   = aws_api_gateway_rest_api.hospital_queue_api.id
-  resource_id   = aws_api_gateway_resource.hospital.id
+  resource_id   = aws_api_gateway_resource.check_queues_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "post_hospital_integration" {
+resource "aws_api_gateway_integration" "submit_integration" {
   rest_api_id             = aws_api_gateway_rest_api.hospital_queue_api.id
-  resource_id             = aws_api_gateway_resource.hospital.id
-  http_method             = aws_api_gateway_method.post_hospital.http_method
+  resource_id             = aws_api_gateway_resource.submit_resource.id
+  http_method             = aws_api_gateway_method.submit_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = insert_lambda_arn
-
-  request_parameters = {
-    "integration.request.header.Content-Type" = "'application/json'"
-  }
-
-  request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
-}
-EOF
-  }
-
-  passthrough_behavior = "WHEN_NO_TEMPLATES"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hospital_queue.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "post_hospital_response" {
-  rest_api_id = aws_api_gateway_rest_api.hospital_queue_api.id
-  resource_id = aws_api_gateway_resource.hospital.id
-  http_method = aws_api_gateway_method.post_hospital.http_method
-  status_code = "200"
+resource "aws_api_gateway_integration" "check_queues_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.hospital_queue_api.id
+  resource_id             = aws_api_gateway_resource.check_queues_resource.id
+  http_method             = aws_api_gateway_method.check_queues_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hospital_queue.invoke_arn
 }
 
-resource "aws_api_gateway_integration_response" "post_hospital_integration_response" {
-  rest_api_id  = aws_api_gateway_rest_api.hospital_queue_api.id
-  resource_id  = aws_api_gateway_resource.hospital.id
-  http_method  = aws_api_gateway_method.post_hospital.http_method
-  status_code  = aws_api_gateway_method_response.post_hospital_response.status_code
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-resource "aws_api_gateway_deployment" "hospital_queue_api" {
+resource "aws_api_gateway_deployment" "hospital_queue_api_deployment" {
   depends_on = [
-    aws_api_gateway_method.post_hospital,
-    aws_api_gateway_integration.post_hospital_integration,
+    aws_api_gateway_integration.submit_integration,
+    aws_api_gateway_integration.check_queues_integration,
   ]
-
   rest_api_id = aws_api_gateway_rest_api.hospital_queue_api.id
   stage_name  = "prod"
 }
