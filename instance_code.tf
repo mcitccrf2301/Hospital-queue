049a2d276a60d30a1524f0500383356caf5a272d
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-08-02 20:56:54 -0400

    Update instance_code.tf

--- instance_code.tf
+++ instance_code.tf
@@ -2,7 +2,8 @@ resource "aws_api_gateway_deployment" "hospital_queue_api" {
   rest_api_id = aws_api_gateway_rest_api.hospital_queue_api.id
   stage_name  = "prod"
   depends_on = [
-    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.submit_integration,
+    aws_api_gateway_integration.check_queues_integration,
   ]
 }
 
