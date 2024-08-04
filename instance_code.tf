4975983cd2ab148947522d042f5b74c2bdbb851e
mcitccrf2301 <mcitccrf2301@gmail.com>
2024-08-04 11:38:46 -0400

    Update instance_code.tf

--- instance_code.tf
+++ instance_code.tf
@@ -18,8 +18,6 @@ data "template_file" "bootstrap_hospital_queue" {
     db_password    = var.db_password
     api_endpoint   = aws_api_gateway_deployment.hospital_queue_api.invoke_url
     api_gateway_url = aws_api_gateway_rest_api.hospital_queue_api.execution_arn
-    hospital       = var.hospital
-    data            = var.data 
   }
 }
 
